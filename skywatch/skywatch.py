c3c91fc2fc9ccca2a8065b3197b3b35f61393a6c
shady <shadybradengithub@shady.icu>
2026-08-11 10:03:22 -0400

    GKA

--- skywatch/skywatch.py
+++ skywatch/skywatch.py
@@ -215,6 +215,12 @@ for ac in data.get('aircraft', []):
         priority = 'default'
         title = "Fighter Jet | " + short_type_value + " | " + desc_value
 
+    if callsign.startswith("GKA"):
+        print(callsign)
+        send_message = 1
+        priority = 'default'
+        title = "Golden Knights Parachuters | " + short_type_value + " | " + desc_value
+
     if send_message == 1: # if we are planning on sending, check ignorelist
         with open("ignorelist.txt", "r") as file:
             for line in file:
