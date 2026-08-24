2d558e31e8a76169086751faa37e166b55c327b0
shady <shadybradengithub@shady.icu>
2026-08-24 16:14:47 -0400

    tmpfs and tty

--- compose_lint.py
+++ compose_lint.py
@@ -55,6 +55,8 @@ KEY_ORDER = [
     "device_cgroup_rules",
     "hostname",
     "security_opt",
+    "tmpfs",
+    "tty",
 ]
 _RANK = {k: i for i, k in enumerate(KEY_ORDER)}
 _UNKNOWN_RANK = len(_RANK)
